4bd2ecdf2432cb9d05d75bfe416034f678abd50a
Pavel Kunyavskiy <kunyavskiy@gmail.com>
2024-12-02 21:33:40 +0100

    Move things around

--- src/bin/NN.rs
+++ src/bin/NN.rs
@@ -1,10 +1,12 @@
 use anyhow::*;
-use adv_code_2024::*;
+use adv_code_2024::token_read::TokenRead;
+use adv_code_2024::wrapper::solve_day;
 
 const TEST: &str = "\
 ";
 
 fn main() -> Result<()> {
+
     fn part1(reader: &mut TokenRead) -> Option<i32> {
         None
     }
@@ -15,6 +17,8 @@ fn main() -> Result<()> {
     solve_day(
         "",
         TEST,
+        None,
+        None,
         None,
         None,
         part1,
